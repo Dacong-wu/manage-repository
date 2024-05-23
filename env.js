07c8a8e889dcede73687d29b4a730143dd78bbb1
Dacong-wu <2420390517@qq.com>
2024-05-23 17:48:37 +0800

    Update env.js
    add GITHUB_TOKEN,GITHUB_OWNER

--- env.js
+++ env.js
@@ -1,5 +1,7 @@
 const envData = {
   BASE_PORT: process.env.BASE_PORT, // 监听端口
-  SERVER_NAME: process.env.SERVER_NAME // 程序名称
+  SERVER_NAME: process.env.SERVER_NAME, // 程序名称
+  GITHUB_TOKEN: process.env.GITHUB_TOKEN, // github token
+  GITHUB_OWNER: process.env.GITHUB_OWNER // github owner
 }
 module.exports = { envData }
