e0f4255de671eca15813b261b07c5d42248db309
Dacong-wu <2420390517@qq.com>
2024-05-23 17:47:37 +0800

    add github router

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
-const router = require('koa-router')()
+const router = require('@koa/router')()
+const github = require('./api/github')
 
 router.get('/', ctx => {
   ctx.body = 'Hello World!'
 })
+router.use('/github', github)
 
 module.exports = router
